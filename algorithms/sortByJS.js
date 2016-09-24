0b5d0f82afbb2e606de1cfb187353324171af7b2
hudk <hdkwd@126.com>
2016-09-24 22:02:56 +0800

    add two algorithms, found error in quick sort.

--- algorithms/sortByJS.js
+++ algorithms/sortByJS.js
@@ -1,3 +1,72 @@
+// compare is a function for compare two number, if the first argument is less than the second, return true, else return false
+
+// bubble sort
+function bubbleSort(arr, compare, begin, end) {
+  var len = arr.length;
+  var b, e;
+  if('number' == typeof begin) {
+    b=begin;
+  } else {
+    b=0;
+  }
+  if('number' == typeof end) {
+    e=end;
+  } else {
+    e=len;
+  }
+  var c=compare||function (a,b) {
+    if(a<b) return true;
+    return false;
+  }
+
+  var tmp;
+  for(var i=0; i<e-b; i++) {
+    for(var j=b; j<e-i; j++) {
+      if(!c(arr[j], arr[j+1])) {
+        tmp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=tmp;
+      }
+    }
+  }
+}
+
+// selection sort
+function selectionSort(arr, compare, begin, end) {
+  var len = arr.length;
+  var b, e;
+  if('number' == typeof begin) {
+    b=begin;
+  } else {
+    b=0;
+  }
+  if('number' == typeof end) {
+    e=end;
+  } else {
+    e=len;
+  }
+  var c=compare||function (a,b) {
+    if(a<b) return true;
+    return false;
+  }
+
+  var min, tmp;
+  for(var i=b;i<=e-1;i++) {
+    min=i;
+    for(var j=i+1;j<=e;j++) {
+      if(c(arr[j],arr[min])) {
+        min=j;
+      }
+    }
+    tmp=arr[i];
+    arr[i]=arr[min];
+    arr[min]=tmp;
+  }
+}
+
+
+
+// TODO there is an error when init with begin and end
 // quick sort
 function quickSort(arr, compare, begin, end) {
   var len = arr.length;
